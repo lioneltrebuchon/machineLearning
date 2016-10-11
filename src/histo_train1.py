Switch labels for plot_all_trains which were in the wrong order, and updated histo_train1 with a more readable and precise histogram (smaller scale, and doesn't count the zeros)

src/histo_train1.py
@@ -1,8 +1,8 @@
-########################################################################
+#################################################################
 
-# Pyhton script which plots the first brain (train1) projected onto the 3 plans
+# Pyhton script which plots the hystograms of the 1st training brain
 
-########################################################################
+#################################################################
 
 import numpy as np
 import nibabel as nib
@@ -18,8 +18,6 @@ import os, sys
 X = 175
 Y = 207
 Z = 175
-maxVal = 2900
-
 ''' #for testing
 X = 10
 Y = 10
@@ -29,26 +27,19 @@ Z = 10
 #relative path
 train1 = nib.load("../data/set_train/train_1.nii")
 
-#path from usb key
-#train1 = nib.load("/run/media/lionelt/04F6-B693/ML/data/set_train/train_1.nii")
 data = train1.get_data()
 
-#black->white colormap
-colormap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap',['black','white'],256)
-
-
-##### calculate color histograph over 1 picture
-
-intList=np.empty(X*Y*Z)
+# store all the non zero values in a 1D list
+intList=[]
 for x in range(X):
     for y in range(Y):
         for z in range(Z):
-			intList[z+(y+x*Y)*Z]=int(round(data[x,y,z]))
-
+			if data[x,y,z]!=0:
+				intList.append(int(data[x,y,z]))
 
 # plot and save figure
 histo = plt.figure(0)
-plt.hist(intList)
+plt.hist(intList, 200)
 plt.title("Histogram of train1")
 plt.xlabel("Values of the 3D brain")
 plt.ylabel("Frequencies")