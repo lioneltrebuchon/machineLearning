ba569e5e7066e84419df73063c49f1e313de8130
Lionel Trebuchon <lionelt@stud2-h57.inf.ethz.ch>
2016-10-11 17:15:27 +0200

    Modified files structure (plots, data and src files) and added .gitignore files, modified readme, added a script which plots the first train brain projected on the 3 plans

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 3. Do we need scypy?
 4. Do we needscikit-learn ?
 5. Do we need pandas ?
+
 Install on linux systems (and brutus/euler servers)
 module load python/2.7.6
 python get-pip.py --user
@@ -32,30 +33,7 @@ pip install scipy
 Peut-être se mettre d'accord dès maintenant sur une syntaxe commune du style :
 import numpy as np
 import nibabel as nib
-import matplotlib as plt
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import sklearn as sk
 from sklearn.linear_model import Lasso
-
-Pour visualiser des images (ne marche pas chez moi (Barthe)) :
-http://nipy.org/nibabel/coordinate_systems.html
-
-We can load up the EPI image to get the image data array:
->>> import nibabel as nib
->>> epi_img = nib.load('downloads/someones_epi.nii.gz')
->>> epi_img_data = epi_img.get_data()
->>> epi_img_data.shape
-(53, 61, 33)
-
-Then we have a look at slices over the first, second and third dimensions of the array.
-import matplotlib.pyplot as plt
-def show_slices(slices):
-   """ Function to display row of image slices """
-   fig, axes = plt.subplots(1, len(slices))
-   for i, slice in enumerate(slices):
-       axes[i].imshow(slice.T, cmap="gray", origin="lower")
-
-slice_0 = epi_img_data[26, :, :]
-slice_1 = epi_img_data[:, 30, :]
-slice_2 = epi_img_data[:, :, 16]
-show_slices([slice_0, slice_1, slice_2])
-plt.suptitle("Center slices for EPI image")  
---
+++ src/plot_train1.py
@@ -0,0 +1,93 @@
+########################################################################
+
+# Pyhton script which plots the first brain (train1) projected onto the 3 plans
+
+########################################################################
+
+import numpy as np
+import nibabel as nib
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+#import sklearn as sk
+#from sklearn.linear_model import Lasso
+
+import os, sys
+
+#'''
+X = 175
+Y = 207
+Z = 175
+
+''' #for testing
+X = 10
+Y = 10
+Z = 10
+'''
+
+#relative path
+#train1 = nib.load("../data/set_train/train_1.nii")
+
+#path from usb key
+train1 = nib.load("/run/media/lionelt/04F6-B693/ML/data/set_train/train_1.nii")
+data = train1.get_data()
+
+#black->white colormap
+colormap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap',['black','white'],256)
+
+##### plot the 1st brain projected on x and y axis
+
+#compute means along z-axis
+mean_z=np.empty([X,Y])
+for x in range(X):
+    for y in range(Y):
+	mean_z[x,y] = 0
+        for z in range(Z):
+            	mean_z[x,y]+=data[x, y, z]
+	mean_z[x,y]/=Z
+
+#plot and save figure
+fig_xy = plt.figure(0)
+img_xy = plt.imshow(mean_z,interpolation='nearest',
+                    cmap=colormap,
+                    origin='lower')
+plt.colorbar(img_xy,cmap=colormap)
+fig_xy.savefig("plots/train1_proj_xy.png")
+
+##### plot the 1st brain projected on x and z axis
+
+#compute means along y-axis
+mean_y=np.empty([X,Z])
+for x in range(X):
+    for z in range(Z):
+	mean_y[x,z] = 0
+        for y in range(Y):
+		mean_y[x,z]+=data[x, y, z]
+	mean_y[x,z]/=Y
+
+#plot and save figure
+fig_xz = plt.figure(1)
+img_xz = plt.imshow(mean_y,interpolation='nearest',
+                    cmap=colormap,
+                    origin='lower')
+plt.colorbar(img_xz,cmap=colormap)
+fig_xz.savefig("plots/train1_proj_xz.png")
+
+##### plot the 1st brain projected on z and y axis
+
+#compute means along x-axis
+mean_x=np.empty([Z,Y])
+for z in range(Z):
+    for y in range(Y):
+	mean_x[z,y] = 0
+        for x in range(Z):
+        	mean_x[z,y]+=data[x, y, z]
+	mean_x[z,y]/=X
+
+#plot and save figure
+fig_zy = plt.figure(2)
+img_zy = plt.imshow(mean_x,interpolation='nearest',
+                    cmap=colormap,
+                    origin='lower')
+plt.colorbar(img_zy,cmap=colormap)
+fig_zy.savefig("plots/train1_proj_zy.png")
