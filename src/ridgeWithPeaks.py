291b78c17c4a820eb8915a7c6e97780231a6d28f
Yannick Wolff <wolffy@public-docking-cx-1547.ethz.ch>
2016-10-28 20:07:29 +0200

    Ridge with concatenated features slice and peaks

--- src/ridgeWithPeaks.py
+++ src/ridgeWithPeaks.py
@@ -16,14 +16,26 @@ from sklearn import linear_model
 from sklearn.linear_model import Lasso
 
 # Input (features and age) of the regression
-features = np.genfromtxt('../results/sliceTrainFeatures32.csv', delimiter=",")
+sliceFeatures = np.genfromtxt('../results/sliceTrainFeatures48.csv', delimiter=",")
+p2x = np.genfromtxt('../results/p2_x.csv', delimiter=",")
+p2y = np.genfromtxt('../results/p2_y.csv', delimiter=",")
+p3x = np.genfromtxt('../results/p3_x.csv', delimiter=",")
+p3y = np.genfromtxt('../results/p3_y.csv', delimiter=",")
+
+features = np.concatenate([sliceFeatures,np.reshape(p2x,[-1,1]),np.reshape(p2y,[-1,1]),np.reshape(p3x,[-1,1]),np.reshape(p3y,[-1,1])],1)
+
 #print(features)
 #features = features[0:-1,0:features.shape[0]]
 age = np.genfromtxt('../data/targets.csv', delimiter="\n")
 
 # Features for the prediction
-toPredictFeatures = np.genfromtxt('../results/sliceTestFeatures32.csv', delimiter=",")
+sliceFeatures = np.genfromtxt('../results/sliceTestFeatures48.csv', delimiter=",")
+p2x = np.genfromtxt('../results/test_p2_x.csv', delimiter=",")
+p2y = np.genfromtxt('../results/test_p2_y.csv', delimiter=",")
+p3x = np.genfromtxt('../results/test_p3_x.csv', delimiter=",")
+p3y = np.genfromtxt('../results/test_p3_y.csv', delimiter=",")
 
+toPredictFeatures = np.concatenate([sliceFeatures,np.reshape(p2x,[-1,1]),np.reshape(p2y,[-1,1]),np.reshape(p3x,[-1,1]),np.reshape(p3y,[-1,1])],1)
 
 def ridgeRegression(alphas, features, age, prediction=False, toPredict=np.empty(1, dtype=int)):
     # More info at :
@@ -58,7 +70,7 @@ print(features.shape)
 print(toPredictFeatures.shape)
 '''
 # compute the regression for several alphas
-alphas = [0.001,0.01,0.1,0.5,1,5,10,50,100]
+alphas = [1]
 #alphas = np.linspace(0.00000000001,0.0000001,10001)
 
 #results = ridgeRegression(alphas, features, age)
@@ -74,9 +86,10 @@ print(predictedAges)
 
 
 # write in a csv file
-result = open('../results/resultRidge3_range32.csv','w')
+result = open('../results/resultRidge3_range48withPeaks.csv','w')
 result.write("ID,Prediction,alpha:,"+str(alpha)+"\n")
 for id in range(TEST):
     #print(id)
     result.write(str(id+1)+","+str(predictedAges[id])+"\n")
 result.close()
+
