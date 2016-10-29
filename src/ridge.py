3ab55e5f35191bcd05c9a121e982565ebbbba912
Bidot <bidot@bidot-VirtualBox.(none)>
2016-10-29 15:00:52 +0200

    aa

--- src/ridge.py
+++ src/ridge.py
@@ -15,12 +15,27 @@ import sklearn as sk
 from sklearn import linear_model
 from sklearn.linear_model import Lasso
 
+# feature1 = np.genfromtxt('../results/p2_x.csv', delimiter="\n")
+# feature2 = np.genfromtxt('../results/p3_x.csv', delimiter="\n")
+# feature3 = np.genfromtxt('../results/p2_y.csv', delimiter="\n")
+# feature4 = np.genfromtxt('../results/p3_y.csv', delimiter="\n")
+#
+# features5 = np.transpose(np.array([feature1, feature2, feature3, feature4]))
+
 # Input (features and age) of the regression
-features = np.transpose(np.genfromtxt('../results/sliceFeatures.csv', delimiter="\n"))
+features = np.genfromtxt('../results/sliceFeatures.csv', delimiter=",")
 age = np.genfromtxt('../data/targets.csv', delimiter="\n")
 
+# print(features5)
+# print(features5.shape)
+# print(age)
+# print(age.shape)
+print(features)
+print(features.shape)
+
+'''
 # Features for the prediction
-toPredictFeatures = np.transpose(np.genfromtxt('../results/test_sliceFeatures.csv', delimiter="\n"))
+# toPredictFeatures = np.transpose(np.genfromtxt('../results/test_sliceFeatures.csv', delimiter="\n"))
 
 def ridgeRegression(alphas, features, age, prediction=False, toPredict=np.empty(1, dtype=int)):
     # More info at :
@@ -46,17 +61,20 @@ def ridgeRegression(alphas, features, age, prediction=False, toPredict=np.empty(
     else:
         return {'Coefficient': modelRidge.coef_, 'Alpha': modelRidge.alpha_, 'Score': scoreFinal, 'Intercept': modelRidge.intercept_}
 
-alphaStart = 0.01
-alphaEnd = 10
-alphaStep = 0.01
+# alphaStart = 0.1
+# alphaEnd = 1
+# alphaStep = 0.1
 
 # compute the regression for several alphas
-alphas = np.linspace(alphaStart, alphaEnd, (alphaEnd-alphaStart)/alphaStep)
-coefficient, alpha, score, intercept, predictedAges = ridgeRegression(alphas, features, age, True, toPredictFeatures)
+# alphas = np.linspace(alphaStart, alphaEnd, (alphaEnd-alphaStart)/alphaStep)
+alphas = [0.1]
+# coefficient, alpha, score, intercept, predictedAges = ridgeRegression(alphas, features, age, True, toPredictFeatures)
+coefficient, alpha, score, intercept = ridgeRegression(alphas, features, age)
 
-# write in a csv file
-fresult = open('../results/resultRidge.csv','w')
-fresult.write("ID,Prediction,alpha:,"+alpha+"\n")
-for id in range(TEST):
-    fresult.write(str(id)+","+str(predictedAges[id])+"\n")
-fresult.close()
+# # write in a csv file
+# fresult = open('../results/resultRidge.csv','w')
+# fresult.write("ID,Prediction,alpha:,"+alpha+"\n")
+# for id in range(TEST):
+#     fresult.write(str(id)+","+str(predictedAges[id])+"\n")
+# fresult.close()
+'''
