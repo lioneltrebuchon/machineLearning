b7e4922d0db6ab94e296ee5435cfefb96b397d33
Yannick Wolff <wolffy@public-docking-cx-3528.ethz.ch>
2016-11-22 19:47:45 +0100

    Euler script for jobs

--- MLP2/src/euler_compute_cubes.sh
+++ MLP2/src/euler_compute_cubes.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 for i in `seq 0 27`
     do bsub -W 08:00 -r -o output_train_cube_features_$((i*10+1)\to$((i*10+10)).txt -R "rusage[mem=8000]" python compute_cube_features.py train $((i*10+1) $((i*10+10)
 done
